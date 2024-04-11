6189686495e1feb2b1c15bf28a16a83f5cd73287
Pieter <p.vanegmond@student.tudelft.nl>
2024-04-11 17:49:51 +0200

    First test passes

--- GoGReview/src/test/java/ReviewTest.java
+++ GoGReview/src/test/java/ReviewTest.java
@@ -1,2 +1,12 @@
+import org.example.Review;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class ReviewTest {
+
+    @Test
+    public void testGetRatingGraphics() {
+        Review review = new Review(1, 2, 3);
+        Assertions.assertTrue(review.getRatingGraphics() == 1);
+    }
 }
